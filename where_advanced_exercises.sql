e5fdc5c4ea2154f84f021d5e3aa8b97a741408c3
Chellyann M. Moreno <Chellyannmoreno@gmail.com>
2023-03-10 13:54:37 -0600

    corrections made

--- where_advanced_exercises.sql
+++ where_advanced_exercises.sql
@@ -15,30 +15,31 @@ where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
 What is the employee number of the top three results. 10200, 10397, 10821 */
 select emp_no,first_name, last_name, gender
 from employees
-where first_name IN ('Irena','Vidya', 'Maya')
+where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya')
 and gender= 'M';
 -- Find all unique last names that start with 'E'.
 select distinct emp_no,first_name, last_name
 from employees
 where last_name like 'E%';
 -- Find all unique last names that start or end with 'E'.
-select distinct emp_no,first_name, last_name
+select distinct  last_name
 from employees
-where last_name like 'E%' or last_name= '%E';
+where last_name like 'e%' or last_name= '%e';
 -- Find all unique last names that end with E, but does not start with E?
-select distinct emp_no,first_name, last_name
+select distinct first_name, last_name
 from employees
-where last_name like 'E%'
-and last_name not like '%E';
+where last_name like '%e'
+and last_name not like 'e%';
 -- Find all unique last names that start and end with 'E'.
 select distinct emp_no,first_name, last_name
 from employees
-where last_name like 'E%'
-and last_name  like '%E';
--- Find all current or previous employees hired in the 90s. Enter a comment with top three employee numbers. 10008, 10011, 10012
+where last_name like 'e%e';
+/*  Find all current or previous employees hired in the 90s. Enter a comment with top three employee numbers. 
+10008, 10011, 10012 */
 select * 
 from employees
 where hire_date between '1990-01-01' and '1999-12-31';
+
 /*Find all current or previous employees born on Christmas. 
 Enter a comment with top three employee numbers. 10078, 10115, 10261 */
 select * 
@@ -51,11 +52,11 @@ from employees
 where hire_date between '1990-01-01' and '1999-12-31'
 and birth_date like ('%12-25');
 -- Find all unique last names that have a 'q' in their last name.
-select distinct emp_no, first_name, last_name
+select distinct emp_no, last_name
 from employees
 where last_name like ('%q%');
 -- Find all unique last names that have a 'q' in their last name but not 'qu'.
-select distinct emp_no, first_name, last_name
+select distinct emp_no,last_name
 from employees
 where last_name like ('%q%')
 and last_name not like ('%qu%');
